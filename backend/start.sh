abd82a2b8da3b8760885f4a699b8ddcd4b6cbf5b
AntiVirus <2409879947@qq.com>
2024-04-16 06:23:46 +0800

    cron

--- backend/start.sh
+++ backend/start.sh
@@ -1,2 +1,3 @@
-/etc/init.d/cron restart && /etc/init.d/cron status
-uwsgi /app/uwsgi.ini
+#!/bin/bash
+
+/etc/init.d/cron restart && /etc/init.d/cron status && /etc/init.d/cron start && /usr/local/bin/uwsgi /app/uwsgi.ini
